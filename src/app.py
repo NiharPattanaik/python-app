e129f479cf95f8e8e9ead38107fb6adc0f4994c5
Nihar Pattanaik <nihar_pattanaik@Nihars-MacBook-Pro.local>
2025-04-29 23:16:50 +0530

    Adds CI Github workflow

--- src/app.py
+++ src/app.py
@@ -9,7 +9,8 @@ def details():
     return jsonify(
         {
             'time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            'hostname' : socket.gethostname()
+            'hostname' : socket.gethostname(),
+            'message': 'You are doing great, human'
 
         }
     )
