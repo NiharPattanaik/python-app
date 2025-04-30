6dc776c8cda9b5ddeccaed8c7a6c73703abd1140
Nihar Pattanaik <nihar_pattanaik@Nihars-MacBook-Pro.local>
2025-04-30 22:20:43 +0530

    Modifies vlues file on CD step=2

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
         {
             'time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
             'hostname' : socket.gethostname(),
-            'message': 'You are doing great, human!!!'
+            'message': 'You are doing great, human!!!, Wow its working'
 
         }
     )
